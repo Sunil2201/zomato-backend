dc306c87e4beae7a5b401fdbabc1cedbf2b6d294
Sunil Ballani <ballanisunil123@gmail.com>
2024-08-04 16:11:51 +0530

    feat: added different controllers for restaurant, menu and review

--- controllers/restaurant.controller.js
+++ controllers/restaurant.controller.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 const fs = require("fs");
 const Restaurant = require("../models/restaurant.model");
 
-const restaurantsData = fs.readFileSync("E:/zomato-backend/data/restaurants.json", "utf8");
+const restaurantsData = fs.readFileSync(
+  "E:/zomato-backend/data/restaurants.json",
+  "utf8"
+);
 const restaurants = JSON.parse(restaurantsData);
 
 const seedRestaurantsData = async () => {
@@ -30,4 +33,109 @@ const seedRestaurantsData = async () => {
   }
 };
 
-module.exports = { seedRestaurantsData };
+const createRestaurant = async (restaurant) => {
+  try {
+    const newRestaurant = new Restaurant(restaurant);
+    const savedRestaurant = await newRestaurant.save();
+    return savedRestaurant;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const readRestaurant = async (restaurantName) => {
+  try {
+    const restaurant = await Restaurant.findOne({ name: restaurantName });
+    if (restaurant) {
+      return restaurant;
+    } else {
+      console.log("Restaurant not found for the given name!");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const readAllRestaurants = async () => {
+  try {
+    const restaurants = await Restaurant.find({});
+    return restaurants;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const readResturantsByCuisine = async (cuisineName) => {
+  try {
+    const restaurantsByGivenCuisineName = await Restaurant.find({
+      cuisine: cuisineName,
+    });
+    return restaurantsByGivenCuisineName;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const updateRestaurant = async (restaurantId, dataToUpdate) => {
+  try {
+    const updatedRestaurant = await Restaurant.findByIdAndUpdate(
+      restaurantId,
+      dataToUpdate,
+      { new: true }
+    );
+    if (updatedRestaurant) {
+      return updatedRestaurant;
+    } else {
+      console.log("Could not find restaurant with the given id!");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const deleteRestaurant = async (restaurantId) => {
+  try {
+    const deletedRestaurant = await Restaurant.findByIdAndUpdate(restaurantId);
+    if (deletedRestaurant) {
+      return deletedRestaurant;
+    } else {
+      console.log("Could not find restaurant with the given id!");
+    }
+  } catch (error) {
+    throw error;
+  }
+};
+
+const searchRestaurantsByLocation = async (restaurantLocation) => {
+  try {
+    const restaurantsForGivenLocation = await Restaurant.find({
+      city: restaurantLocation,
+    });
+    return restaurantsForGivenLocation;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const filterRestaurantsByRating = async (rating) => {
+  try {
+    const restaurantsFilteredByRating = await Restaurant.find({
+      averageRating: { $gte: rating },
+    });
+    return restaurantsFilteredByRating;
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = {
+  seedRestaurantsData,
+  createRestaurant,
+  readRestaurant,
+  readAllRestaurants,
+  readResturantsByCuisine,
+  updateRestaurant,
+  deleteRestaurant,
+  searchRestaurantsByLocation,
+  filterRestaurantsByRating
+};
--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dbConnection();
 // seedRestaurantsData();
 // seedUsersData();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
